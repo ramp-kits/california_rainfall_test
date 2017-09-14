f79e9b864e0e7911b9c95f4ba0094158dcce236d
David John Gagne <djgagne@ou.edu>
2017-09-14 14:13:04 -0600

    Fixed starting kit

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -3,7 +3,7 @@ from sklearn.base import BaseEstimator
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.clf = LogisticRegression(C=0.01, penalty="l1")
+        self.clf = LogisticRegression(C=0.001, penalty="l1")
 
     def fit(self, X, y):
         self.clf.fit(X, y)
