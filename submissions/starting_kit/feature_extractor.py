588dbc889bdb24f4d13f6132de99d9335b56bf5c
David John Gagne <djgagne@ou.edu>
2017-10-23 09:46:00 -0600

    Fixed issue with feature extractor not importing numpy

--- submissions/starting_kit/feature_extractor.py
+++ submissions/starting_kit/feature_extractor.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import pandas as pd
+import numpy as np
 from sklearn.decomposition import PCA
 
 class FeatureExtractor():
@@ -18,6 +19,7 @@ class FeatureExtractor():
                 self.sds[var][self.sds[var] == 0] = 1
             var_norm = (X_ds[var] - self.means[var]) / self.sds[var]
             var_flat = var_norm.stack(latlon=("lat", "lon")).values
+            del var_norm
             var_flat[np.isnan(var_flat)] = 0
             self.pca[var] = PCA(n_components=self.num_comps)
             self.pca[var].fit(var_flat)
